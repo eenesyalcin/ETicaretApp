b09fd4ceb4a782770798d165fe4328521bb67bd8
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-07-31 10:26:59 +0300

    User collection was created.

---
+++ backend/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+// Oluşturacağımız collection için şema oluşturduk.
+const userShema = new mongoose.Schema({
+    _id: String,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: Boolean,
+    createdDate: Date
+})
+
+// Bir User(collection) oluşturduk.
+const User = mongoose.model("User", userShema);
+
+// Oluşturduğumuz User'ın dışarıdan erişilebilmesini sağladık.
+module.exports = User;
