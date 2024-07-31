b11878544eef82be2b73dfbd511cc9abf0f56769
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-07-31 11:59:29 +0300

    Login API method was written.

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -12,16 +12,14 @@ const options = {
 };
 
 
+// REGISTER API
 router.post("/register", async(req, res) => {
     try {
-
-
         // Body içerisinde gelen bilgilerin oluşturduğumuz User collection bilgileri ile eşleşmesini sağlar.
         const user = new User(req.body);   
         user._id = uuidv4();                // Verilen id değerinin unique bir değer olmasını sağlar.
         user.createdDate = new Date();
         user.isAdmin = false;
-
 
         // Kayıt yapılırken email'in unique olmasını sağlayan kontrolü yazdık.
         const checkUserEmail = await User.findOne({email: user.email});
@@ -34,10 +32,30 @@ router.post("/register", async(req, res) => {
         let model = {token: token, user: user};
         res.json(model);
         }
-        
-
     } catch (error) {
         // API isteğinde hata olduğunda(status = 500) geriye hata mesajı döndürür.
+        res.status(500).json({message: error.message});
+    }
+})
+
+
+// LOGIN API
+router.post("/login", async(req, res) => {
+    try {
+        const {email, password} = req.body;
+        let user = await User.findOne({email: email});
+        if(user == null){
+            res.status(403).json({message: "Kullanıcı bulunamadı!"});
+        }else{
+            if(user.password != password){
+                res.status(403).json({message: "Şifre yanlış!"});
+            }else{
+                const token = jwt.sign({}, secretKey, options);
+                let model = {token: token, user: user};
+                res.json(model);
+            }
+        }
+    } catch (error) {
         res.status(500).json({message: error.message});
     }
 })
