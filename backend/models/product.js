f4250a7921fc7a3ac78f5bceb7cc1d181f8ae11f
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-08-02 17:42:38 +0300

    Product model was created.

---
+++ backend/models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// Oluşturacağımız Product collection için şema oluşturduk.
+const productSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    imageUrls: Array,
+    stock: Number,
+    price: Number,
+    createDate: Date,
+    isActive: Boolean,
+    categories: [{type: String, ref: "Category"}]
+});
+
+// Şemadan Product(collection) oluşturduk.
+const Product = mongoose.model("Product", productSchema);
+
+// Oluşturduğumuz Product'ın dışarıdan erişilebilmesini sağladık.
+module.exports = Product;
