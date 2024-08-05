b46fc3a782f1ce047b970f97a0f37ea4f0bd3e53
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-08-05 11:01:20 +0300

    Product deletion method was written.

--- backend/routers/product.router.js
+++ backend/routers/product.router.js
@@ -8,7 +8,6 @@ const response = require("../services/response.service");
 
 // ÜRÜN EKLEME
 router.post("/add", upload.array("images"), async(req, res) => {
-
     response(res, async() => {
         const {name, stock, price, categories} = req.body;      // Gelen ürün bilgilerini body içerisinde aldık.
         const productId = uuidv4();                             // Verilen id değerinin unique bir değer olmasını sağlar.
@@ -28,5 +27,23 @@ router.post("/add", upload.array("images"), async(req, res) => {
         // Ürünün kaydedilmesini sağlar.
         await product.save();
         res.json({message: "Ürün kaydı başarıyla tamamlandı."});
+    });
+});
+
+
+// ÜRÜN SİLME
+router.post("removeById", async(req, res) => {
+    response(res, async() => {
+        const {_id} = req.body;     // Body içerisinde id gelir.
+
+        // Burada ürünü bulup, o ürüne ait olan tüm resimleri siliyoruz.
+        const product = await Product.findById(_id);
+        for(const image of product.imageUrls){
+            fs.unlink(image.path, () => {});
+        }
+
+        // Ürünün silinmesini sağlar.
+        await Product.findByIdAndRemove(_id);
+        res.json({message: "Ürün kaydı başarıyla silindi."});
     });
 });
